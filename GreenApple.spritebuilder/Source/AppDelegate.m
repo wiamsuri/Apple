a76efd6b9440a3de981e3708dc326e2a6dfc9b63
Watt Iamsuri <wiamsuri@gmail.com>
2014-08-27 19:52:10 -0500

    mod research files

--- GreenApple.spritebuilder/Source/AppDelegate.m
+++ GreenApple.spritebuilder/Source/AppDelegate.m
@@ -51,6 +51,8 @@
     //badge number to 0
     [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
     
+    
+    
     // Configure Cocos2d with the options set in SpriteBuilder
     NSString* configPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Published-iOS"]; // TODO: add support for Published-Android support
     configPath = [configPath stringByAppendingPathComponent:@"configCocos2d.plist"];
